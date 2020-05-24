15c4ae12a1238a197bf54bbceb4c1405e901f0c7
brunnerf <brunner_felix@bluewin.ch>
2020-05-24 18:27:26 +0200

    add graphql server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-exports.handler = async () => {
-    return {
-        body: 'Hello from Lambda Dev\n'
-    };
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+const typeDefs = gql`
+  type Query {
+    user: User
+  }
+
+  type User {
+    id: ID
+    name: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    user: () => ({ id: 123, name: 'Felix' })
+  }
 };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+exports.handler = server.createHandler();
